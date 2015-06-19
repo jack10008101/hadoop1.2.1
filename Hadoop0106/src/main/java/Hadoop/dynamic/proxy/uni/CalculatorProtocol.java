62e08917a8d99ba25178d105ce9b11d78227825d
jack10008101 <1538490988@qq.com>
2015-06-19 10:59:29 +0800

    Revert "关于hadoop的相关代码，主要是hadoop design pattern的代码"
    This reverts commit 65b0a10e901470efe114d4edd52526c5d6b57678.

--- Hadoop0106/src/main/java/Hadoop/dynamic/proxy/uni/CalculatorProtocol.java
+++
@@ -1,6 +0,0 @@
-package Hadoop.dynamic.proxy.uni;
-
-public interface CalculatorProtocol {
-	public int add(int a,int b);
-	public int subtract(int a,int b);
-}
--- Hadoop0106/src/main/java/com/web/datamining/project/MatrixMapper.java
+++
@@ -1,5 +0,0 @@
-package com.web.datamining.project;
-
-public class MatrixMapper {
-
-}
